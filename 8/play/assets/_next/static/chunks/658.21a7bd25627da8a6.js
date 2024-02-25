dc853b5d3075e948229b454739dcb70887944ca7
ObambaCaree <133160850+ObambaCaree@users.noreply.github.com>
2024-02-25 08:29:47 -0500

    Create 658.21a7bd25627da8a6.js

---
+++ 8/play/assets/_next/static/chunks/658.21a7bd25627da8a6.js
@@ -0,0 +1,195 @@
+"use strict";
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[658], {
+    28761: function(n, e, t) {
+        t.r(e),
+        t.d(e, {
+            default: function() {
+                return X
+            }
+        });
+        var i, o, a, r, d, p, l, s = t(11720), c = t(59930), x = t(79354), h = t(55475), g = t(12812), f = t(53574), m = t(57762), u = t(94381), w = t(39426), b = t(25617), y = t(77691), k = t(95509), v = t(78603), F = t(50912), Z = t(72742), z = t(78943), j = t(50445), P = function(n, e) {
+            return Object.defineProperty ? Object.defineProperty(n, "raw", {
+                value: e
+            }) : n.raw = e,
+            n
+        }, B = j.ZP.div(i || (i = P(["\n  display: flex;\n  align-items: center;\n"], ["\n  display: flex;\n  align-items: center;\n"]))), C = (0,
+        j.ZP)(B)(o || (o = P(["\n  position: sticky;\n  top: 0;\n  z-index: 3;\n  justify-content: space-between;\n\n  color: ", ";\n  background-color:  rgba(0, 0, 0, .6);\n  padding: 18px 36px;\n\n  i {\n    cursor: pointer;\n  }\n\n  &.home {\n    padding-top: 0;\n    padding-bottom: 0;\n    @media only screen and (max-width: 1032px) {\n      padding-top: 0;\n      padding-bottom: 0;\n    }\n  }\n\n  .header-search {\n    position: relative;\n    transform: none;\n    left: auto;\n    top: auto;\n    flex: 0 0 min(30%, 400px);\n    margin-top: auto;\n    margin-bottom: auto;\n    > div {\n      width: auto;\n    }\n  }\n  &::before {\n    content: '';\n    position: absolute;\n    top: 0;\n    left: 0;\n    width: 100%;\n    height: 100%;\n    backdrop-filter: blur(24px);\n  }\n\n  @media only screen and (max-width: 1032px) {\n    padding: 20px 24px;\n\n    & > div {\n      z-index: 1;\n      display: flex;\n      align-items: center;\n      justify-content: space-between;\n    }\n  }\n"], ["\n  position: sticky;\n  top: 0;\n  z-index: 3;\n  justify-content: space-between;\n\n  color: ", ";\n  background-color:  rgba(0, 0, 0, .6);\n  padding: 18px 36px;\n\n  i {\n    cursor: pointer;\n  }\n\n  &.home {\n    padding-top: 0;\n    padding-bottom: 0;\n    @media only screen and (max-width: 1032px) {\n      padding-top: 0;\n      padding-bottom: 0;\n    }\n  }\n\n  .header-search {\n    position: relative;\n    transform: none;\n    left: auto;\n    top: auto;\n    flex: 0 0 min(30%, 400px);\n    margin-top: auto;\n    margin-bottom: auto;\n    > div {\n      width: auto;\n    }\n  }\n  &::before {\n    content: '';\n    position: absolute;\n    top: 0;\n    left: 0;\n    width: 100%;\n    height: 100%;\n    backdrop-filter: blur(24px);\n  }\n\n  @media only screen and (max-width: 1032px) {\n    padding: 20px 24px;\n\n    & > div {\n      z-index: 1;\n      display: flex;\n      align-items: center;\n      justify-content: space-between;\n    }\n  }\n"])), (function(n) {
+            return n.theme.colors.white80
+        }
+        )), A = (0,
+        j.ZP)(B)(a || (a = P(["\n  gap: 8px;\n  z-index: 1;\n  position: relative;\n  width: fit-content;\n\n  @media only screen and (max-width: 981px) and (orientation: portrait),\n  (orientation: landscape) and (max-height: 575.98px) {\n    .icon-info {\n      display: none;\n    }\n  }\n"], ["\n  gap: 8px;\n  z-index: 1;\n  position: relative;\n  width: fit-content;\n\n  @media only screen and (max-width: 981px) and (orientation: portrait),\n  (orientation: landscape) and (max-height: 575.98px) {\n    .icon-info {\n      display: none;\n    }\n  }\n"]))), N = j.ZP.span(r || (r = P(["\n  display: inline-block;\n  width: 22px;\n  height: 22px;\n\n  @media only screen and (min-width: 981px) {\n    display: none;\n  }\n"], ["\n  display: inline-block;\n  width: 22px;\n  height: 22px;\n\n  @media only screen and (min-width: 981px) {\n    display: none;\n  }\n"]))), _ = j.ZP.nav(d || (d = P(["\n  margin-left: auto;\n  position: relative;\n  display: flex;\n  align-items: center;\n\n  @media only screen and (max-width: 981px) and (orientation: portrait) {\n    background: rgba(0, 0, 0, 0.7);\n    backdrop-filter: blur(60px);\n    position: fixed;\n    bottom: 0;\n    padding: 14px;\n    left: 0;\n    right: 0;\n    justify-content: space-between;\n    z-index: 4;\n    padding-right: 14px !important;\n  }\n  .sep {\n    border: 1px solid rgba(255, 255, 255, 0.2);\n    height: 20px;\n    width: 0px;\n    margin: 0 16px;\n  }\n  p {\n    color: rgba(255, 255, 255, 0.7);\n    margin: 0;\n    font-size: 12px;\n    line-height: 18px;\n    white-space: nowrap;\n  }\n  a:not(.tiktok) {\n    font-size: 14px;\n    line-height: 150%;\n    color: #FAFAFB;\n    text-decoration: none;\n    color: #FAFAFB;\n    display: flex;\n    align-items: center;\n\n    .icon-union {\n      font-size: 11px;\n      margin-left: 5px;\n    }\n\n    &:hover {\n      color: ", ";\n    }\n  }\n\n  > div {\n    margin-left: 12px;\n  }\n"], ["\n  margin-left: auto;\n  position: relative;\n  display: flex;\n  align-items: center;\n\n  @media only screen and (max-width: 981px) and (orientation: portrait) {\n    background: rgba(0, 0, 0, 0.7);\n    backdrop-filter: blur(60px);\n    position: fixed;\n    bottom: 0;\n    padding: 14px;\n    left: 0;\n    right: 0;\n    justify-content: space-between;\n    z-index: 4;\n    padding-right: 14px !important;\n  }\n  .sep {\n    border: 1px solid rgba(255, 255, 255, 0.2);\n    height: 20px;\n    width: 0px;\n    margin: 0 16px;\n  }\n  p {\n    color: rgba(255, 255, 255, 0.7);\n    margin: 0;\n    font-size: 12px;\n    line-height: 18px;\n    white-space: nowrap;\n  }\n  a:not(.tiktok) {\n    font-size: 14px;\n    line-height: 150%;\n    color: #FAFAFB;\n    text-decoration: none;\n    color: #FAFAFB;\n    display: flex;\n    align-items: center;\n\n    .icon-union {\n      font-size: 11px;\n      margin-left: 5px;\n    }\n\n    &:hover {\n      color: ", ";\n    }\n  }\n\n  > div {\n    margin-left: 12px;\n  }\n"])), (function(n) {
+            return n.theme.colors.ascent
+        }
+        )), R = j.ZP.a(p || (p = P(["\n  text-decoration: none;\n  width: 33px;\n  margin-left: 12px;\n  .hover {\n    opacity: 0;\n    border-radius: 8px;\n  }\n  img {\n    max-width: 100%;\n  }\n  &:hover {\n    img:not(.hover) {\n      display: none;\n    }\n    .hover {\n      opacity: 1;\n    }\n  }\n"], ["\n  text-decoration: none;\n  width: 33px;\n  margin-left: 12px;\n  .hover {\n    opacity: 0;\n    border-radius: 8px;\n  }\n  img {\n    max-width: 100%;\n  }\n  &:hover {\n    img:not(.hover) {\n      display: none;\n    }\n    .hover {\n      opacity: 1;\n    }\n  }\n"]))), I = j.ZP.a(l || (l = P(["\n  display: none;\n  padding: 7px 16px;\n  justify-content: center;\n  align-items: center;\n  gap: 4px;\n  text-decoration: none;\n  font-weight: 500;\n\n  border: 1px solid #FFFFFF;\n  border-radius: 8px;\n  font-size: 13px;\n  color: #FFFFFFCC;\n\n  &:hover {\n    color: #fff !important;\n  }\n  @media only screen and (max-width: 768px) and (orientation: portrait) {\n    display: flex;\n  }\n"], ["\n  display: none;\n  padding: 7px 16px;\n  justify-content: center;\n  align-items: center;\n  gap: 4px;\n  text-decoration: none;\n  font-weight: 500;\n\n  border: 1px solid #FFFFFF;\n  border-radius: 8px;\n  font-size: 13px;\n  color: #FFFFFFCC;\n\n  &:hover {\n    color: #fff !important;\n  }\n  @media only screen and (max-width: 768px) and (orientation: portrait) {\n    display: flex;\n  }\n"]))), T = t(92284), E = t(97997), L = function(n) {
+            var e = n.extLink
+              , t = n.type
+              , i = n.id
+              , o = (0,
+            s.useState)(!1)
+              , a = o[0]
+              , r = o[1];
+            return (0,
+            E.BX)(R, {
+                className: "d-desk-flex",
+                id: i,
+                href: e,
+                target: "_blank",
+                rel: "noreferrer",
+                onMouseEnter: function() {
+                    return r(!0)
+                },
+                onMouseLeave: function() {
+                    return r(!1)
+                },
+                children: [(0,
+                E.tZ)("img", {
+                    fetchpriority: "low",
+                    width: 32,
+                    height: 32,
+                    alt: "now.gg ".concat(t),
+                    src: "".concat(u.u1, "social/").concat(t, ".svg")
+                }), a && (0,
+                E.tZ)("img", {
+                    alt: "now.gg ".concat(t),
+                    className: "hover",
+                    src: "".concat(u.u1, "social/").concat(t, ".gif")
+                })]
+            })
+        }, X = function(n) {
+            var e = n.loginResolved
+              , t = (0,
+            s.useState)(!1)
+              , i = t[0]
+              , o = t[1]
+              , a = (0,
+            s.useRef)(null)
+              , r = (0,
+            s.useRef)(null)
+              , d = (0,
+            b.I0)();
+            return (0,
+            s.useEffect)((function() {
+                d((0,
+                k.J)())
+            }
+            ), []),
+            (0,
+            E.tZ)(C, {
+                className: "home",
+                as: "header",
+                style: {
+                    minHeight: 64
+                },
+                children: (0,
+                E.BX)(h.W2, {
+                    style: {
+                        display: "flex",
+                        gap: "20px"
+                    },
+                    children: [(0,
+                    E.tZ)(A, {
+                        children: (0,
+                        E.tZ)(Z.Z, {
+                            hasTagline: !1,
+                            showInfoIcon: !1
+                        })
+                    }), (0,
+                    E.tZ)(T.Z, {
+                        wrapperClass: "header-search"
+                    }), (0,
+                    E.tZ)(N, {
+                        ref: r,
+                        onClick: function() {
+                            return o(!0)
+                        },
+                        children: (0,
+                        E.tZ)(x.ol, {
+                            style: {
+                                height: "100%",
+                                width: "100%",
+                                marginRight: "0"
+                            }
+                        })
+                    }), (0,
+                    E.tZ)(g.Z, {
+                        toggle: i,
+                        exitingDelay: .3,
+                        children: (0,
+                        E.tZ)(f.Z, {
+                            origin: "browse",
+                            close: function() {
+                                return o(!1)
+                            }
+                        })
+                    }), (0,
+                    E.BX)(_, {
+                        ref: a,
+                        children: [(0,
+                        E.BX)("a", {
+                            href: z.j7,
+                            target: "_blank",
+                            rel: "noreferrer",
+                            children: ["Developers", " ", (0,
+                            E.tZ)(m.mo, {
+                                children: (0,
+                                E.tZ)(c.Z, {
+                                    name: "union",
+                                    size: 9
+                                })
+                            })]
+                        }), (0,
+                        E.tZ)("span", {
+                            className: "sep d-desk-flex"
+                        }), (0,
+                        E.tZ)("p", {
+                            className: "d-desk-flex",
+                            children: "Follow us on"
+                        }), (0,
+                        E.tZ)(L, {
+                            extLink: u.ul,
+                            type: "discord",
+                            id: "ng-discord-btn"
+                        }), (0,
+                        E.tZ)(L, {
+                            extLink: "https://www.youtube.com/channel/UCJ56IoBJr4vtNrtTw0CEpYg",
+                            type: "youtube",
+                            id: "ng-youtube-btn"
+                        }), (0,
+                        E.tZ)(L, {
+                            extLink: "https://www.tiktok.com/@nowgg_games",
+                            type: "tiktok",
+                            id: "ng-tiktok-btn"
+                        }), (0,
+                        E.BX)(I, {
+                            className: "tiktok",
+                            target: "_blank",
+                            rel: "noreferrer",
+                            href: "https://www.tiktok.com/@nowgg_games",
+                            children: [(0,
+                            E.tZ)("img", {
+                                fetchpriority: "high",
+                                width: 25,
+                                height: 25,
+                                alt: "now.gg TikTok",
+                                src: "".concat(u.u1, "social/tiktok.svg")
+                            }), "Follow us on TikTok"]
+                        }), (0,
+                        E.tZ)(v.r, {
+                            loading: null,
+                            persistor: F.D,
+                            children: (0,
+                            y.nI)() && (0,
+                            s.createPortal)((0,
+                            E.tZ)(w.ProfilesAction, {
+                                topbarRef: a,
+                                searchIconRef: r,
+                                loginResolved: e
+                            }), document.body)
+                        })]
+                    })]
+                })
+            })
+        }
+    }
+}]);
+//# sourceMappingURL=658.21a7bd25627da8a6.js.map
