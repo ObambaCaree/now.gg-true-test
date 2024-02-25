dcbed68c16ad57edf4248dfe594f639f9a275802
ObambaCaree <133160850+ObambaCaree@users.noreply.github.com>
2024-02-25 08:31:35 -0500

    Create 762.53765f12c4a34ea5.js

---
+++ 8/play/assets/_next/static/chunks/762.53765f12c4a34ea5.js
@@ -0,0 +1,123 @@
+"use strict";
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[762], {
+    57762: function(n, t, e) {
+        e.d(t, {
+            il: function() {
+                return k
+            },
+            aI: function() {
+                return P
+            },
+            r2: function() {
+                return Z
+            },
+            W2: function() {
+                return z
+            },
+            xg: function() {
+                return j
+            },
+            mo: function() {
+                return _
+            },
+            __: function() {
+                return X
+            },
+            X2: function() {
+                return C
+            },
+            qp: function() {
+                return E
+            },
+            LD: function() {
+                return M
+            },
+            ao: function() {
+                return N
+            },
+            C0: function() {
+                return O
+            },
+            vx: function() {
+                return S
+            },
+            xv: function() {
+                return q
+            },
+            cw: function() {
+                return D
+            },
+            xR: function() {
+                return I
+            }
+        });
+        var i, o, r, a, l, p, s, c, d, u, x, h, f, g, m, w, b = e(50445), v = e(45628), y = function(n, t) {
+            return Object.defineProperty ? Object.defineProperty(n, "raw", {
+                value: t
+            }) : n.raw = t,
+            n
+        }, k = (0,
+        b.iv)(i || (i = y(["\n    @media screen and (orientation: landscape) and (max-height: 300px),\n    (orientation: portrait) and (max-width: 300px) {\n        height: 100%;\n    }\n\n    @media screen and (max-width: 400px) {\n        width: 100%;\n    }\n    max-width: 440px;\n"], ["\n    @media screen and (orientation: landscape) and (max-height: 300px),\n    (orientation: portrait) and (max-width: 300px) {\n        height: 100%;\n    }\n\n    @media screen and (max-width: 400px) {\n        width: 100%;\n    }\n    max-width: 440px;\n"]))), P = (0,
+        b.iv)(o || (o = y(["\n  ", "\n"], ["\n  ", "\n"])), k), Z = (0,
+        b.iv)(r || (r = y(["\n    padding: 20px;\n"], ["\n    padding: 20px;\n"]))), z = b.ZP.div(a || (a = y(["\n    padding: 24px;\n    .link {\n        font-size: 12px;\n        text-align: center;\n        display: flex;\n        align-items: center;\n        color: ", "\n    }\n"], ["\n    padding: 24px;\n    .link {\n        font-size: 12px;\n        text-align: center;\n        display: flex;\n        align-items: center;\n        color: ", "\n    }\n"])), (function(n) {
+            return n.theme.colors.base900
+        }
+        )), j = b.ZP.div(l || (l = y(["\n    display: flex;\n    justify-content: space-between;\n    height: 45px;\n"], ["\n    display: flex;\n    justify-content: space-between;\n    height: 45px;\n"]))), _ = b.ZP.span(p || (p = y(["\n   margin-left: 4px;\n"], ["\n   margin-left: 4px;\n"]))), X = b.ZP.label(s || (s = y(["\n    position: relative;\n    display: inline-block;\n    width: 30px;\n    height: 18px;\n    margin-left: 8px;\n    > input {\n        visibility: hidden;\n    }\n    > span {\n        position: absolute;\n        cursor: pointer;\n        top: 0;\n        left: 0;\n        right: 0;\n        bottom: 0;\n        -webkit-transition: .4s;\n        transition: .4s;\n        border-radius: ", ";\n        border: 1px solid ", ';\n        width: 24px;\n        height: 12px;\n        margin: auto 0px;\n    }\n    > span:before {\n        position: absolute;\n        content: "";\n        height: 80%;\n        aspect-ratio: 1;\n        left: 10%;\n        top: 10%;\n        background-color: ', ";\n        -webkit-transition: .4s;\n        transition: .4s;\n        border-radius: 50%;\n    }\n    > input:checked + span {\n        background-color: ", ";\n        &:before {\n            background-color: ", ";\n        }\n    }\n    > input:checked + span:before {\n        transform: translateX(130%);\n    }\n"], ["\n    position: relative;\n    display: inline-block;\n    width: 30px;\n    height: 18px;\n    margin-left: 8px;\n    > input {\n        visibility: hidden;\n    }\n    > span {\n        position: absolute;\n        cursor: pointer;\n        top: 0;\n        left: 0;\n        right: 0;\n        bottom: 0;\n        -webkit-transition: .4s;\n        transition: .4s;\n        border-radius: ", ";\n        border: 1px solid ", ';\n        width: 24px;\n        height: 12px;\n        margin: auto 0px;\n    }\n    > span:before {\n        position: absolute;\n        content: "";\n        height: 80%;\n        aspect-ratio: 1;\n        left: 10%;\n        top: 10%;\n        background-color: ', ";\n        -webkit-transition: .4s;\n        transition: .4s;\n        border-radius: 50%;\n    }\n    > input:checked + span {\n        background-color: ", ";\n        &:before {\n            background-color: ", ";\n        }\n    }\n    > input:checked + span:before {\n        transform: translateX(130%);\n    }\n"])), (function(n) {
+            return n.theme.border.radius12
+        }
+        ), (function(n) {
+            return n.theme.colors.white
+        }
+        ), (function(n) {
+            return n.theme.colors.white
+        }
+        ), (function(n) {
+            return n.theme.colors.ascent
+        }
+        ), (function(n) {
+            return n.theme.colors.white
+        }
+        )), C = b.ZP.div(c || (c = y(["\n    display: flex;\n    flex-direction: row;\n    color: ", ";\n    margin: 8px 0px 0px 0px;\n    span, a {\n        color: ", " !important;\n    }\n     .linkStyle {\n        text-align: center;\n        color: ", ";\n        font-size: 12px;\n    }\n"], ["\n    display: flex;\n    flex-direction: row;\n    color: ", ";\n    margin: 8px 0px 0px 0px;\n    span, a {\n        color: ", " !important;\n    }\n     .linkStyle {\n        text-align: center;\n        color: ", ";\n        font-size: 12px;\n    }\n"])), (function(n) {
+            return n.theme.colors.white
+        }
+        ), (function(n) {
+            return n.theme.colors.white
+        }
+        ), (function(n) {
+            return n.theme.colors.white
+        }
+        )), E = b.ZP.div(d || (d = y(["\n    display: flex;\n    justify-content: space-between;\n    padding-top: ", ";\n    color: ", ";\n\n    span, a {\n        color: ", " !important;\n    }\n"], ["\n    display: flex;\n    justify-content: space-between;\n    padding-top: ", ";\n    color: ", ";\n\n    span, a {\n        color: ", " !important;\n    }\n"])), (function(n) {
+            return n.theme.spacing.base4
+        }
+        ), (function(n) {
+            return n.theme.colors.white
+        }
+        ), (function(n) {
+            return n.theme.colors.white
+        }
+        )), M = b.ZP.div(u || (u = y(["\n    border-radius: 8px;\n    background: ", ";\n    padding: 18px;\n    margin-bottom: 16px;\n\n    > div {\n        display: flex;\n        align-items: center;\n\n        > div:first-child {\n            flex-basis: 44px;\n            height: 44px;\n        }\n        ", "\n\n    }\n\n    img {\n        width: 44px;\n        height: 44px;\n    }\n\n    button {\n        width: 100px;\n    }\n"], ["\n    border-radius: 8px;\n    background: ", ";\n    padding: 18px;\n    margin-bottom: 16px;\n\n    > div {\n        display: flex;\n        align-items: center;\n\n        > div:first-child {\n            flex-basis: 44px;\n            height: 44px;\n        }\n        ", "\n\n    }\n\n    img {\n        width: 44px;\n        height: 44px;\n    }\n\n    button {\n        width: 100px;\n    }\n"])), (function(n) {
+            return n.theme.colors.white10
+        }
+        ), (function(n) {
+            var t = n.orientation
+              , e = n.isMobile;
+            return t === v.cj.portrait && e && "\n            flex-direction: column;\n            justify-content: center;\n            tex-align: center;\n            gap: 8px;\n        "
+        }
+        )), N = b.ZP.div(x || (x = y(["\n    display: flex;\n    flex-direction: column;\n    transform: translateX(18px);\n    max-width: 50%;\n\n\n    ", "\n"], ["\n    display: flex;\n    flex-direction: column;\n    transform: translateX(18px);\n    max-width: 50%;\n\n\n    ", "\n"])), (function(n) {
+            var t = n.orientation
+              , e = n.isMobile;
+            return t === v.cj.portrait && e && "\n        align-items: center;\n        transform: none;\n        max-width: 100%;\n        "
+        }
+        )), O = b.ZP.span(h || (h = y(["\n    font-size: 12px;\n    font-weight: 400;\n    line-height: 150%;\n    color: ", ";\n    text-overflow: ellipsis;\n    overflow: hidden;\n"], ["\n    font-size: 12px;\n    font-weight: 400;\n    line-height: 150%;\n    color: ", ";\n    text-overflow: ellipsis;\n    overflow: hidden;\n"])), (function(n) {
+            return n.theme.colors.white60
+        }
+        )), S = b.ZP.p(f || (f = y(["\n    font-size: 12px;\n    font-weight: 600;\n    line-height: 150%;\n    text-overflow: ellipsis;\n    overflow: hidden;\n    margin: 0;\n    color: ", ";\n    display: flex;\n"], ["\n    font-size: 12px;\n    font-weight: 600;\n    line-height: 150%;\n    text-overflow: ellipsis;\n    overflow: hidden;\n    margin: 0;\n    color: ", ";\n    display: flex;\n"])), (function(n) {
+            return n.theme.colors.white
+        }
+        )), q = b.ZP.div(g || (g = y(["\n    a {\n        color: rgba(255, 255, 255, 0.8);\n    }\n"], ["\n    a {\n        color: rgba(255, 255, 255, 0.8);\n    }\n"]))), D = b.ZP.div(m || (m = y(["\n    font-weight: 400;\n    font-size: 12px;\n    line-height: 150%;\n    text-decoration: underline;\n    cursor: pointer;\n    color: ", ";\n"], ["\n    font-weight: 400;\n    font-size: 12px;\n    line-height: 150%;\n    text-decoration: underline;\n    cursor: pointer;\n    color: ", ";\n"])), (function(n) {
+            return n.theme.colors.white
+        }
+        )), I = b.ZP.div(w || (w = y(["\n    display: flex;\n"], ["\n    display: flex;\n"])))
+    }
+}]);
+//# sourceMappingURL=762.53765f12c4a34ea5.js.map
