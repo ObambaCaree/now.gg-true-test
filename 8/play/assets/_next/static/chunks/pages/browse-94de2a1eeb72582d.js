9dd65924b32977b5cfef90bb77166c5fed8d73a2
ObambaCaree <133160850+ObambaCaree@users.noreply.github.com>
2024-02-25 08:23:58 -0500

    Create browse-94de2a1eeb72582d.js

---
+++ 8/play/assets/_next/static/chunks/pages/browse-94de2a1eeb72582d.js
@@ -0,0 +1,19 @@
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[518], {
+    7930: function(n, _, u) {
+        (window.__NEXT_P = window.__NEXT_P || []).push(["/browse", function() {
+            return u(63995)
+        }
+        ])
+    }
+}, function(n) {
+    n.O(0, [774, 164, 806, 804, 166, 888, 179], (function() {
+        return _ = 7930,
+        n(n.s = _);
+        var _
+    }
+    ));
+    var _ = n.O();
+    _N_E = _
+}
+]);
+//# sourceMappingURL=browse-94de2a1eeb72582d.js.map
